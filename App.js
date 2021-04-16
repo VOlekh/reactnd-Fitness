ae2dc5c810d0f5941a9b8f59b1716a920d08996e
Valentina-Olekhnovich <volekh@gmail.com>
2021-04-16 15:26:27 +0200

    add AddEntry

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import * as React from "react"
+import { Text } from "react-native"
+import AddEntry from "./components/AddEntry"
 
-export default function App() {
+export default class App extends React.Component {
+  render(){
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Universal React with Expo</Text>
+      <AddEntry/ >
     </View>
   );
+}
 }
