25a52fdc1d9f35b779c21dbf1af66cced9032846
Valentina-Olekhnovich <volekh@gmail.com>
2021-04-19 21:16:19 +0200

    add increment decrement

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Text } from "react-native"
+import { View } from "react-native"
 import AddEntry from "./components/AddEntry"
 
 export default class App extends React.Component {
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,11 +4,49 @@ import { getMetricMetaInfo } from "../utils/helpers";
 
 
 export default class AddEntry extends Component {
-    render() {
+    state = {
+        //metrics
+        run:0,
+        bike: 0,
+        swime: 0,
+        //
+        sleep:0,
+        eat: 0,
+    }
+    //create increment method
+    increment = (metric) =>{
+        //max what should metric to be and a step to increment - step
+        const{max, step}= getMetricMetaInfo(metric)
+        this.setState(()=>{
+            const count = statate[metric] + step
+        //object you return from setState is gona be merjed with the current state
+            return{
+                //keep all we have
+                ...state,
+                //update metric with if : count is bigger than max value, than count
+                [metric]:count > max ? max : count
+            }
+        })
+    }
+    decrement = (metric) =>{
+        this.setState((state) => {
+            const count = state[metric] - getMetricMetaInfo(metric).step
+            return {
+                ...state,
+                [metric]: count > max ? max : count,
+            }
+        })
+    }
+    slide = (metric, value) => {
+      this.setState(() => ({
+        [metric]: value
+      }))
+    }
+    render(){
       return (
         <View>
             {getMetricMetaInfo("bike").getIcon()}
         </View>
       )
     }
-  }
+}   
