31ec8a358beded6737ccd11d177a33c1b1c10210
Valentina-Olekhnovich <volekh@gmail.com>
2021-05-13 17:21:17 +0200

    add Navigation, combine style

--- App.js
+++ App.js
@@ -16,6 +16,119 @@ import Constants from "expo-constants";
 import Title from "./components/Title";
 import Live from "./components/Live";
 import { setLocalNotification } from "./utils/helpers";
+import { createStackNavigator } from '@react-navigation/stack';
+import EntryDetail from './components/EntryDetails';
+
+const Stack = createStackNavigator();
+function Tabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={
+        ({ route }) => ({
+          tabBarIcon: ({ color }) => {
+            if (route.name === 'History') {
+              return <Ionicons name='ios-bookmarks' size={30} color={color} />
+            } else if (route.name === 'AddEntry') {
+              return <FontAwesome name='plus-square' size={30} color={color} />
+            } else if (route.name === 'Live') {
+            return <Ionicons name='ios-speedometer' size={30} color={color} />
+          }
+          },
+        })}
+      tabBarOptions={{
+        activeTintColor: Platform.OS === 'ios' ? purple : white,
+        style: {
+          height: 56,
+          backgroundColor: Platform.OS === 'ios' ? white : purple,
+          shadowColor: 'rgba(0, 0, 0, 0.24)',
+          shadowOffset: {
+            width: 0,
+            height: 3
+          },
+          shadowRadius: 6,
+          shadowOpacity: 1
+        }
+      }}>
+      <Tab.Screen
+        name="History"
+        component={History}
+        options={{ headerShown: false }} />
+      <Tab.Screen
+        name="AddEntry"
+        component={AddEntry}
+        options={{ headerShown: false }} />
+      <Tab.Screen
+          name="Live"
+          component={Live}
+          options={{ headerShown: false }} />
+    </Tab.Navigator>
+  )
+}
+
+{/* <Tab.Navigator
+    initialRouteName="History"
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ color }) => {
+        if (route.name === "History") {
+          return (
+            <Ionicons name="ios-bookmarks" size={30} color={color} />
+          );
+        } else if (route.name === "AddEntry") {
+          return (
+            <FontAwesome name="plus-square" size={30} color={color} />
+          );
+        } else if (route.name === "Live") {
+          return (
+            <Ionicons
+              name="ios-speedometer"
+              size={30}
+              color={color}
+            />
+          );
+        }
+      },
+    })}
+    tabBarOptions={{
+      activeTintColor: Platform.OS === "ios" ? purple : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === "ios" ? white : purple,
+        shadowColor: "rgba(0, 0, 0, 0.24)",
+        shadowOffset: {
+          width: 0,
+          height: 3,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1,
+      },
+    }}
+    >
+    <Tab.Screen
+      name="Add Entry"
+      component={AddEntry}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <FontAwesome name="plus-square" size={30} color={color} />
+        ),
+      }}
+    />
+
+    <Tab.Screen
+      name="History"
+      component={History}
+      options={{
+        tabBarIcon: ({ color }) => (
+          <Ionicons name="ios-bookmarks" size={30} color={color} />
+        ),
+      }}
+    />
+
+    <Tab.Screen
+      name="Live"
+      component={Live}
+      options={{ headerShown: false }}
+    />
+</Tab.Navigator> */}
 
 function UdaciStatusBar({ backgroundColor, ...props }) {
   return (
@@ -46,70 +159,22 @@ export default class App extends React.Component {
         <NavigationContainer>
           <View style={{ flex: 1 }}>
             <UdaciStatusBar backgroundColor={purple} style='light' />
-            <Tab.Navigator
-              initialRouteName="History"
-              screenOptions={({ route }) => ({
-                tabBarIcon: ({ color }) => {
-                  if (route.name === "History") {
-                    return (
-                      <Ionicons name="ios-bookmarks" size={30} color={color} />
-                    );
-                  } else if (route.name === "AddEntry") {
-                    return (
-                      <FontAwesome name="plus-square" size={30} color={color} />
-                    );
-                  } else if (route.name === "Live") {
-                    return (
-                      <Ionicons
-                        name="ios-speedometer"
-                        size={30}
-                        color={color}
-                      />
-                    );
-                  }
-                },
-              })}
-              tabBarOptions={{
-                activeTintColor: Platform.OS === "ios" ? purple : white,
-                style: {
-                  height: 56,
-                  backgroundColor: Platform.OS === "ios" ? white : purple,
-                  shadowColor: "rgba(0, 0, 0, 0.24)",
-                  shadowOffset: {
-                    width: 0,
-                    height: 3,
-                  },
-                  shadowRadius: 6,
-                  shadowOpacity: 1,
-                },
-              }}
-            >
-              <Tab.Screen
-                name="Add Entry"
-                component={AddEntry}
-                options={{
-                  tabBarIcon: ({ color }) => (
-                    <FontAwesome name="plus-square" size={30} color={color} />
-                  ),
-                }}
-              />
-
-              <Tab.Screen
-                name="History"
-                component={History}
-                options={{
-                  tabBarIcon: ({ color }) => (
-                    <Ionicons name="ios-bookmarks" size={30} color={color} />
-                  ),
-                }}
-              />
-
-              <Tab.Screen
-                name="Live"
-                component={Live}
-                options={{ headerShown: false }}
-              />
-            </Tab.Navigator>
+            <Stack.Navigator>
+              <Stack.Screen
+                name="Home"
+                component={Tabs} />
+              <Stack.Screen
+                  name="EntryDetail"
+                  component={EntryDetail}
+                  options={({ route }) => ({
+                    headerTintColor: white,
+                    headerStyle: {
+                      backgroundColor: purple
+                    },
+                    title: route.params.entryId,
+                    headerTitle: props => <Title {...props} />
+                  })} />
+            </Stack.Navigator>
           </View>
         </NavigationContainer>
       </Provider>
